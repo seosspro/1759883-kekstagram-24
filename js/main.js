3ae88b7921fcf9a7b0c5af728358dad79c9e9ebf
Дмитрий <kdimonr@mail.ru>
2021-10-06 12:51:33 +0700

    Объекты и функции

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+const AUTHOR_NAMES = [
+  "Дмитрий",
+  "Евгений",
+  "Максим",
+  "Сергей",
+  "Анна",
+  "Валерий",
+];
+
+const LIKES = {
+  min: 5,
+  max: 10,
+};
+
+const COMMENTS = {
+  min: 1,
+  max: 5,
+};
+
+const COMMENT_LIST = [`Всё отлично!`, `В целом всё неплохо. Но не всё.`];
+
 function getRandom(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -7,3 +28,17 @@ function getRandom(min, max) {
 function getRandomArrayElement(array) {
   return array[getRandom(0, array.length - 1)];
 }
+
+const getCommentsArray = function (randomAmount) {
+  const resultComments = [];
+
+  for (let i = 0; i < randomAmount; i++) {
+    resultComments.push({
+      avatar: `img/avatar-${getRandom(1, 6)}.svg`,
+      message: getRandomArrayElement(COMMENTS_LIST),
+      name: getRandomArrayElement(AUTHOR_NAMES),
+    });
+  }
+
+  return resultComments;
+};

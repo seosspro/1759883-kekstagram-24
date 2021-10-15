c03d53ef8c15781789f5c196c356bd02bb8c90b0
Дмитрий <kdimonr@mail.ru>
2021-10-15 16:34:49 +0700

    импорт и экспорт файлов

--- js/main.js
+++ js/main.js
@@ -1,23 +1,4 @@
-const AUTHOR_NAMES = [
-  'Дмитрий',
-  'Евгений',
-  'Максим',
-  'Сергей',
-  'Анна',
-  'Валерий',
-];
-
-const LIKES = {
-  min: 5,
-  max: 10,
-};
-
-const COMMENTS = {
-  min: 1,
-  max: 5,
-};
-
-const COMMENTS_LIST = ['Всё отлично!', 'В целом всё неплохо. Но не всё.'];
+import { AUTHOR_NAMES, LIKES, COMMENTS, COMMENTS_LIST } from './util.js';
 
 function getRandom(min, max) {
   min = Math.ceil(min);
